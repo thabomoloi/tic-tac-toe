94934f0ed6a24fc83f15b55260407abc399f2e0d
Thabo Moloi <thabomoloicodes@gmail.com>
2022-09-01 07:31:20 +0200

    Add method to check if a shape wins

--- js/script.js
+++ js/script.js
@@ -10,8 +10,21 @@ const Player = (aName, aShape) => {
 const GameBoard = () => {
     const gameboard = ["", "", "", "", "", "", "", "", ""];
     const isWin = (shape) => {
-        for (let i = 0; i < 9; i++) {
-
+        // check if shape appears 3 times in a row
+        for (let i in [0, 3, 6]) {
+            if (gameboard[i] == shape && gameboard[i + 1] == shape && gameboard[i + 2] == shape)
+                return true;
         }
+        // check if shape appears 3 times in a column
+        for (let i in [0, 1, 2]) {
+            if (gameboard[i] == shape && gameboard[i + 3] == shape && gameboard[i + 6] == shape)
+                return true;
+        }
+        // check if shape appears 3 times in a diagonal
+        for (let i in [0, 2]) {
+            if (gameboard[i] == shape && gameboard[4] == shape && gameboard[8 - i] == shape)
+                return true;
+        }
+        return false;
     }
 }
