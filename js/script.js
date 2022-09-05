5ac3410f4ed8ac4ddc3ec8e05ea635ede339d2ea
Thabo Moloi <thabomoloicodes@gmail.com>
2022-09-05 14:51:28 +0200

    Add game controls

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Contains methods for finding move of a computer
+ */
 const findMoves = (() => {
     /**
      * Gets the move in an easy mode.
@@ -36,21 +39,73 @@ const findMoves = (() => {
     return { findMove };
 })();
 
-const TicTacToe = (gamemode, letter) => {
-    const human = letter;
-    const computer = letter === "X" ? "O" : "X";
-    const mode = gamemode;
-    const board = ["", "", "", "", "", "", "", "", ""];
-
+/**
+ * contains methods for checking whether the game is over and the winner.
+ */
+const game = (() => {
     /**
      * Check if a board is full.
+     * @param {string[]} board 
+     * @returns true if the board is full, else false.
      */
-    const isBoardFull = () => {
+    const isBoardFull = (board) => {
         for (let i = 0; i < board.length; i++)
             if (board[i] === "")
                 return false;
         return true;
     }
+    /**
+     * Check if a player has won.
+     * @param {string} letter 
+     * @param {string} board 
+     */
+    const isWin = (letter, board) => {
+        // Rows
+        if ((board[0] == letter && board[1] == letter && board[2] == letter) ||
+            (board[3] == letter && board[4] == letter && board[5] == letter) ||
+            (board[6] == letter && board[7] == letter && board[8] == letter)) {
+            return true;
+        }
+        // Columns
+        if ((board[0] == letter && board[3] == letter && board[6] == letter) ||
+            (board[1] == letter && board[4] == letter && board[7] == letter) ||
+            (board[2] == letter && board[5] == letter && board[8] == letter)) {
+            return true;
+        }
+        // Diagonals 
+        if ((board[0] == letter && board[4] == letter && board[8] == letter) ||
+            (board[2] == letter && board[4] == letter && board[6] == letter)) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * checks if there is a tie.
+     * @param {string[]} board 
+     */
+    const isTie = (board) => {
+        if (isWin("X") || isWin("O"))
+            return false;
+        if (!isBoardFull(board))
+            return false;
+        return true;
+    }
+    /**
+     * Check if the game is over.
+     * @param {string[]} board 
+     */
+    const isGameOver = (board) => {
+        return (isWin("X", board) || isWin("O", board) || isTie(board));
+    }
+    return { isWin, isTie, isGameOver };
+});
+
+const TicTacToe = () => {
+    const human = "X";
+    const computer = "O";
+    const mode = "easy"
+    const board = ["", "", "", "", "", "", "", "", ""];
+
     /**
      * Sets the letter of the player
      * @param {string} letter 
