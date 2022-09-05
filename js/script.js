97210d9798589475292e6f736b6ebe6e19d3ab16
Thabo Moloi <thabomoloicodes@gmail.com>
2022-09-05 12:51:27 +0200

    Add find move method

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
+/**
+ * Gets the move in an easy mode.
+ * @param {string[]} gameboard 
+ */
+const easyPlay = (gameboard) => {
 
+}
+/**
+ * 
+ * @param {string[]} gameboard 
+ * @param {string} letter
+ */
+const mediumPlay = (gameboard, letter) => {
 
+}
+/**
+ * Finds the move of a computer.
+ * @param {string[]} gameboard
+ * @param {string} letter 
+ * @param {string} mode 
+ */
+const findMove = (gameboard, letter, mode) => {
+
+}
