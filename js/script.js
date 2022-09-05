268d1d73a13356eb03f019f678b38fb60123e5a2
Thabo Moloi <thabomoloicodes@gmail.com>
2022-09-05 14:05:34 +0200

    Put functions inside find moves

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
-/**
- * Gets the move in an easy mode.
- * Looks for an empty random space.
- * @param {string[]} gameboard 
- */
-const easyPlay = (gameboard) => {
-    const empty = [];
-    for (let i = 0; i < gameboard.length; i++) {
-        if (gameboard[i] === "")
-            empty.push(i);
+const FindMoves = () => {
+    /**
+     * Gets the move in an easy mode.
+     * Looks for an empty random space.
+     * @param {string[]} gameboard 
+     */
+    const easyPlay = (gameboard) => {
+        const empty = [];
+        for (let i = 0; i < gameboard.length; i++) {
+            if (gameboard[i] === "")
+                empty.push(i);
+        }
+        return empty[Math.floor(Math.random() * empty.length)];
     }
-    return empty[Math.floor(Math.random() * empty.length)];
-}
-/**
- * 
- * @param {string[]} gameboard 
- * @param {string} letter
- */
-const mediumPlay = (gameboard, letter) => {
+    /**
+     * 
+     * @param {string[]} gameboard 
+     * @param {string} letter
+     */
+    const mediumPlay = (gameboard, letter) => {
 
-}
-/**
- * Finds the move of a computer.
- * @param {string[]} gameboard
- * @param {string} letter 
- * @param {string} mode 
- */
-const findMove = (gameboard, letter, mode) => {
-    if (mode === "easy")
-        return easyPlay(gameboard);
+    }
+    /**
+     * Finds the move of a computer.
+     * @param {string[]} gameboard
+     * @param {string} letter 
+     * @param {string} mode 
+     */
+    const findMove = (gameboard, letter, mode) => {
+        if (mode === "easy")
+            return easyPlay(gameboard);
+        if (mode === "medium")
+            return mediumPlay(gameboard, letter);
+    }
+
+    return { findMove };
 }
